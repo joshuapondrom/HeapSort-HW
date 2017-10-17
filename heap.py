499ea0cd9cc6a784685cca8bc03e38f432423cf0
joshuapondrom <japrkf@mst.edu>
2017-10-16 22:55:19 -0500

    Added visual heap sort. Stores pictures to be made into animations

--- heap.py
+++ heap.py
@@ -1,42 +1,31 @@
-import matplotlib.pyplot as plt
+import pylab
 from time import sleep
 from random import randint
 from heapq import *
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
-xs = []
-ys = []
-
 a = 0
 b = 100
-
-def display(array):
-	xs.append(range(len(array)))
-	ys.append(array)
-	#plot.bar(range(len(array)),array)
 
 def fillarray(array):
 	return[randint(a,b) for i in range(len(array))]
 
 def heapsort(array):
+	imgidx = 0
 	heap = []
 	sortedarray = []
 	for element in array:
 		heappush(heap, element)
 	for i in range(len(heap)):
 		sortedarray.append(heappop(heap))
+		pylab.plot(range(len(sortedarray)),sortedarray,'k.',markersize=6)
+		pylab.savefig("heapsort/img" +'%04d' %imgidx +".png")
+		pylab.clf()
+		imgidx = imgidx + 1
 		print('Sorted: {} Unsorted Heap: {}'.format(sortedarray,heap))
-		display(sortedarray)
 	return sortedarray
 
 array = [9,8,7,6,5,4,3,2,1]#*100
-display(array)
 array = fillarray(array)
 print(array)
 array = heapsort(array)
 print(array)
-ax1.clear()
-ax1.plot(xs,ys)
-ani = animation.FuncAnimation(fig, animate, interval=1000)
-plt.show()
