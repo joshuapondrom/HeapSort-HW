89c08442923cdbcdef581a2b23e5db4ec4ec547f
joshuapondrom <japrkf@mst.edu>
2017-10-16 22:46:24 -0500

    Broke some stuff

--- heap.py
+++ heap.py
@@ -1,20 +1,20 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 from time import sleep
 from random import randint
 from heapq import *
 
-plot.ion()
-plot.show()
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+xs = []
+ys = []
 
 a = 0
 b = 100
 
 def display(array):
-	plot.clf()
-	sleep(0.1)
-	plot.bar(range(len(array)),array)
-	plot.draw()
-	sleep(0.1)
+	xs.append(range(len(array)))
+	ys.append(array)
+	#plot.bar(range(len(array)),array)
 
 def fillarray(array):
 	return[randint(a,b) for i in range(len(array))]
@@ -32,9 +32,11 @@ def heapsort(array):
 
 array = [9,8,7,6,5,4,3,2,1]#*100
 display(array)
-sleep(2)
 array = fillarray(array)
 print(array)
 array = heapsort(array)
 print(array)
-sleep(2)
+ax1.clear()
+ax1.plot(xs,ys)
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
