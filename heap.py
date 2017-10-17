2f071ed0854c5861a875b4689c7a990ddb787e1a
joshuapondrom <japrkf@mst.edu>
2017-10-17 00:24:50 -0500

    Invariants and correctness

--- heap.py
+++ heap.py
@@ -3,9 +3,13 @@ from time import sleep
 from random import randint
 from heapq import *
 
+#lowest val
 a = 0
+#highest val
 b = 100
+#number of elements
 n = 100
+#for storing visualization
 img = 1
 
 def fillarray(array):
@@ -14,12 +18,14 @@ def fillarray(array):
 def swap(array, first, second):
 	array[first], array[second] = array[second], array[first]
 
-def heapify(array, end, i):
+def maxheapify(array, end, i):
+	#visualization vv
 	global img
 	pylab.plot(range(len(array)),array,'k.',markersize=6)
 	pylab.savefig("heapsort/img" + '%04d' %img + ".png")
 	pylab.clf()
 	img = img + 1
+	#visualization ^^
 
 	left = 2 * i + 1
 	right = 2 * (i + 1)
@@ -30,16 +36,29 @@ def heapify(array, end, i):
 		maxvalue = right
 	if maxvalue != i:
 		swap(array, i, maxvalue)
-		heapify(array, end, maxvalue)	
+		maxheapify(array, end, maxvalue)	
 
 def heapsort(array):
+	#Invariant: array is a unsorted list
 	end = len(array)
+	# '//' is integer division in python
 	start = end // 2 - 1
+	#This loop makes the array into a max heap
 	for i in range(start, -1, -1):
-		heapify(array, end, i)
+		maxheapify(array, end, i)
+	#Array is a max heap right now
+	#This loop goes through the elements of the heap and puts them
+	# at the end, then does not heapify the elements taken out
 	for i in range(end-1, 0, -1):
+		#Invariant: array[0] is the maximum value of a heap
+		#           which is array[0] through array[i]
 		swap(array, i, 0)
-		heapify(array, i, 0)
+		#Invariant: maximum value of heap is now moved out
+		#	    of the heap and into the sorted section
+		maxheapify(array, i, 0)
+		#Invariant: array[0] through array[i] now satisfies
+		#           heap conditions
+	#Invariant: Heap is now empty and there is a fully sorted list
 	return array
 
 #def heapsort(array):
